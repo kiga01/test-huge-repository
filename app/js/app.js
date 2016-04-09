e56b4dd9720393d694a29b2287f52f1728d8196b
kiga01 <ahinestrosa1984@gmail.com>
2016-04-08 20:22:15 -0500

    responsive menu

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,15 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function myButtonFunction() {
+    document.getElementById("myDropdownButton").classList.toggle("show-mobile");
+}
+
+function windowHeight() {
+    var windowHeight = document.body.clientHeight;
+    document.getElementsByClassName("navigation-menu-mobile").style.height = "500px";
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -15,4 +24,16 @@ window.onclick = function(event) {
             }
         }
     }
+    if (!event.target.matches('.dropbtn-mobile')) {
+        console.log('test');
+        var dropdowns = document.getElementsByClassName("navigation-menu-mobile");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-mobile')) {
+                openDropdown.classList.remove('show-mobile');
+            }
+        }
+    }
 }
+
